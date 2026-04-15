9b6651b421b6c1b75e18232c1adadd9565598739
Peyton Ford <peytonford7@gmail.com>
2026-04-15 06:29:53 -0500

    Auth Middleware

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,17 +1,19 @@
 import { Router } from 'express';
 import homeRoutes from './homeRoutes.js';
-import authRoutes from './authRoutes.js';
 import userRoutes from './userRoutes.js';
+import authRoutes from './authRoutes.js';
 import fileRoutes from './fileRoutes.js';
 import panelRoutes from './panelRoutes.js';
 import boardRoutes from './boardRoutes.js';
 import errRoutes from './errRoutes.js';
+import authMiddleware from '../middleware/authMiddleware';
 
 const router = Router();
 
 router.use('/', homeRoutes);
-router.use('/', authRoutes);
 router.use('/', userRoutes);
+router.use(authMiddleware);
+router.use('/', authRoutes);
 router.use('/', fileRoutes);
 router.use('/', panelRoutes);
 router.use('/', boardRoutes);
